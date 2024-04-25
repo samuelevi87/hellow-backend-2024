c88def9444b9252d3ca8855cc7612dc96fbe60e3
IRIONIR JOSE EBERT FILHO <irionir_ebert-filho@estudante.sesisenai.org.br>
2024-04-25 17:08:00 -0300

    Iniciando o Diário de Bordo e Aula Variáveis e tipos de dados

---
+++ variaveis-tipos-dados-irionir-filho/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        int anoAtual = 2024;
+        int anoNascimento = 2010;
+        int idade = anoAtual-anoNascimento;
+        String nome = "irionir filho";
+        System.out.println("meu nome é" + nome);
+        System.out.println( "idade: " + idade );
+
+    }
+}
