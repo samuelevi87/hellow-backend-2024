57491f8021f151a256a34a3705fb27ab1b7d1347
ALAN MOISES CRISTOVÃO <alan_cristovao@estudante.sesisenai.org.br>
2024-04-25 16:58:50 -0300

    Inicio do diário de bordo e variáveis e tipods de dados

---
+++ variaveis-tipos-dados-alan-cristovao-/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Começando em Java");
+        int anoAtual = 2024;
+        int anoNacimento = 2009;
+        int idade = anoAtual-anoNacimento;
+        System.out.println("Idade: " + idade);
+        String nome = "Alan cristovão";
+        System.out.println("Meu nome é: " + nome);
+    }
+}
