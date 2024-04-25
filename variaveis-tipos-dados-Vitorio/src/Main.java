39f233f76a3fc8d586cd01f56604c9787abe2f54
VITÓRIO DE MOURA <vitorio_moura@estudante.sesisenai.org.br>
2024-04-25 17:12:48 -0300

    Iniciando o Diário de Bordo e Aula de Variáveis e Tipos de dados

---
+++ variaveis-tipos-dados-Vitorio/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Começando em Java");
+
+        int anoAtual = 2024;
+
+        int anoNascimento = 2010;
+
+        int idade = anoAtual - anoNascimento;
+        String nome = "Vitório ";
+        System.out.println("idade: " + idade);
+
+        System.out.println("Ninguém me vence no kour.io.");
+    }
+}
