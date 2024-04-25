a2ecf46cb1b898899c9a4aacd0f3a0358c8acde6
JOHANN CHRISTIAN HELLER <johann_heller@estudante.sesisenai.org.br>
2024-04-25 16:58:47 -0300

    inicio do diario de bordo, e variáveis e tipos de dados

---
+++ variaveis-tipos-dados-johann-heller/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Começando Em Java!");
+    int anoAtual = 2024;
+    int anoNacscimento = 2010;
+    int idade = anoAtual-anoNacscimento;
+    String nome = "Johann Heller";
+        System.out.println("meu nome é: "+nome);
+        System.out.println("idade: "+idade);
+    }
+}
