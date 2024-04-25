b1b63fd8f0ee2b83d5a90a195e050f3dff6e3bc9
GABRIEL HENRIQUE PAMPLONA <gabriel_h_pamplona@estudante.sesisenai.org.br>
2024-04-25 17:03:08 -0300

    diario dia 25

---
+++ variaveis-tipos-dados-gabriel-pamplona/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("começando em java");
+        int anoAtual = 2024;
+        int anoNascimento = 2009;
+        int idade = anoAtual-anoNascimento;
+        String nome = "gabriel pamplona";
+
+        System.out.println("meu nome é: " + nome);
+        System.out.println("Idade: " + idade);
+    }
+}
