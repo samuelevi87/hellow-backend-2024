85719452a6115eecea2979d8d4dbf0e79b960323
VITOR LEANDRO NESTOR DA FONSECA <vitor_fonseca@estudante.sesisenai.org.br>
2024-04-25 17:06:08 -0300

    iniciando o Diario de Bordo e variaveis e tipos de dados

---
+++ variaveis-tipos-dados-vitor-fonseca/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Começando Em Java");
+        int anoAtual = 2024;
+        int anoNascimento = 2011;
+        int idade = anoAtual-anoNascimento;
+        String nome = "Vitor Leandro";
+        System.out.println("Meu nome è: " + nome);
+        System.out.println("idade: " + idade);
+    }
+}
