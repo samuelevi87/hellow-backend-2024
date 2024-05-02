70d1c73a03740696a6be88dfe10142c682de9bf1
ALAN MOISES CRISTOVÃO <alan_cristovao@estudante.sesisenai.org.br>
2024-05-02 17:04:21 -0300

    mudança na lista de exercicíos do java

--- listaDeExercicios-Alan/src/Main.java
+++ listaDeExercicios-Alan/src/Main.java
@@ -1,5 +1,25 @@
 public class Main {
     public static void main(String[] args) {
+        ExercicioJava.exercicio01();
+        ExercicioJava.exercicio02();
+        ExercicioJava.exercicio03();
+        ExercicioJava.exercicio04();
+        ExercicioJava.exercicio05();
+        ExercicioJava.exercicio06();
+        ExercicioJava.exercicio07();
+        ExercicioJava.exercicio08();
+        ExercicioJava.exercicio09();
+        ExercicioJava.exercicio10();
+        ExercicioJava.exercicio11();
+        ExercicioJava.exercicio12();
+        ExercicioJava.exercicio13();
         ExercicioJava.exercicios14();
+        ExercicioJava.exercicios15();
+        ExercicioJava.exercicios16();
+        ExercicioJava.exercicios17();
+        ExercicioJava.exercicios18();
+        ExercicioJava.exercicios19();
+        ExercicioJava.exercicios20();
+
     }
 }
