cb3ac26d903be6fd7958a8a6a78a10eb54a72636
Samuel Levi <68257385+samuelevi87@users.noreply.github.com>
2024-05-23 14:55:31 -0300

    Update ExerciciosJava.java

--- ListaDeExercicios/src/ExerciciosJava.java
+++ ListaDeExercicios/src/ExerciciosJava.java
@@ -1,2 +1,158 @@
 public class ExerciciosJava {
+    /**
+     * Declare uma variável do tipo inteiro chamada "idade" e atribua a ela o valor 25. Em seguida, imprima
+     * a idade com uma mensagem adequada.
+     */
+    public static void exercicio01() {
+       
+    }
+
+    /**
+     * Declare uma variável do tipo double chamada "peso" e atribua a ela o valor 68.5. Em seguida, imprima o peso com uma mensagem adequada.
+     */
+    public static void exercicio02() {
+    
+    }
+
+    /**
+     * Declare uma variável do tipo char chamada "primeiraLetra" e atribua a ela o valor 'A'. Em seguida, imprima a primeira letra com uma mensagem adequada.
+     */
+    public static void exercicio03() {
+      
+    }
+
+    /**
+     * Declare uma variável do tipo boolean chamada "verdadeiro" e atribua a ela o valor true. Em seguida, imprima se é verdadeiro ou não com uma mensagem adequada.
+     */
+    public static void exercicio04() {
+       
+    }
+
+    /**
+     * Declare duas variáveis do tipo inteiro, "numero1" e "numero2", e atribua a elas os valores 10 e 20, respectivamente. Em seguida, calcule e imprima a soma desses dois números.
+     */
+    public static void exercicio05() {
+       
+    }
+
+    /**
+     * Declare uma variável do tipo double chamada "precoProduto" e atribua a ela o valor 59.99. Em seguida, imprima o preço do produto com uma mensagem adequada.
+     */
+    public static void exercicio06() {
+        
+
+    }
+
+    /**
+     * Declare uma variável do tipo char chamada "letra" e atribua a ela o valor 'B'. Em seguida, imprima a letra com uma mensagem adequada.
+     */
+    public static void exercicio07() {
+      
+
+    }
+
+    /**
+     * Declare uma variável do tipo boolean chamada "falso" e atribua a ela o valor false. Em seguida, imprima se é falso ou não com uma mensagem adequada.
+     */
+    public static void exercicio08() {
+        
+
+    }
+
+    /**
+     * Declare uma variável do tipo String chamada "nome" e atribua a ela o seu nome. Em seguida, imprima o nome com uma mensagem adequada.
+     */
+    public static void exercicio09() {
+      
+
+    }
+
+    /**
+     * Declare uma variável do tipo inteiro chamada "quantidadeAlunos" e atribua a ela um valor inicial de 30. Em seguida, imprima a quantidade de alunos com uma mensagem adequada.
+     */
+    public static void exercicio10() {
+       
+
+    }
+
+    /**
+     * Declare uma variável do tipo inteiro chamada "numeroDeEstudantes" e atribua a ela um valor inicial de 20. Em seguida, imprima o número de estudantes com uma mensagem adequada.
+     */
+    public static void exercicio11() {
+       
+
+    }
+
+    /**
+     * Declare uma variável do tipo double chamada "saldoBancario" e atribua a ela um valor inicial de 1500.75. Em seguida, imprima o saldo bancário com uma mensagem adequada.
+     */
+    public static void exercicio12() {
+       
+    }
+
+    /**
+     * Declare uma variável do tipo char chamada "opcaoMenu" e atribua a ela um valor inicial de 'C'. Em seguida, imprima a opção do menu com uma mensagem adequada.
+     */
+    public static void exercicio13() {
+       
+
+    }
+
+    /**
+     * Declare uma variável do tipo boolean chamada "solBrilha" e atribua a ela o valor true. Em seguida, imprima se o sol está brilhando ou não com uma mensagem adequada.
+     */
+    public static void exercicio14() {
+       
+
+    }
+
+    /**
+     * Declare duas variáveis do tipo inteiro, "largura" e "altura", e atribua a elas os valores 5 e 10, respectivamente. Em seguida, calcule e imprima a área de um
+     * retângulo usando essas variáveis.
+     */
+    public static void exercicio15() {
+       
+
+    }
+
+    /**
+     * Declare uma variável do tipo double chamada "temperatura" e atribua a ela o valor 25.5. Em seguida, imprima a temperatura com uma mensagem adequada.
+     */
+    public static void exercicio16() {
+       
+
+    }
+
+    /**
+     * Declare uma variável do tipo char chamada "sexo" e atribua a ela o valor 'F'. Em seguida, imprima o sexo da pessoa com uma mensagem adequada.
+     */
+    public static void exercicio17() {
+        
+
+    }
+
+    /**
+     * Declare uma variável do tipo boolean chamada "chovendo" e atribua a ela o valor false. Em seguida, imprima se está chovendo ou não com uma mensagem adequada.
+     */
+    public static void exercicio18() {
+        
+
+    }
+
+    /**
+     * Declare uma variável do tipo String chamada "frase" e atribua a ela uma frase sobre Java. Em seguida, imprima a frase com uma mensagem adequada.
+     */
+    public static void exercicio19() {
+       
+
+    }
+
+    /**
+     * Declare uma variável do tipo inteiro chamada "idadeDoUsuario" e atribua a ela o valor 30. Em seguida, imprima a idade do usuário com uma mensagem adequada.
+     */
+    public static void exercicio20() {
+      
+
+    }
+
 }
